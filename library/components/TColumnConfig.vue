e1ad351762127b7830b6f1266b2245908a287a63
meg <meg@topcoatdata.com>
2022-02-28 15:21:26 -0500

    Added TColumnConfig.vue

---
+++ library/components/TColumnConfig.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "TColumnConfig",
+  props: {
+    columnConfig: {
+      type: Array,
+      default() {
+        return {};
+      },
+    },
+    setColumnConfig: {
+      type: Function,
+    },
+  },
+  created() {
+    if (typeof this.setColumnConfig === "function") {
+      this.setColumnConfig(this.columnConfig);
+    }
+  },
+};
+</script>
