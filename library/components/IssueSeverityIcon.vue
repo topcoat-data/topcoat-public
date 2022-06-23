05e07667d4a2be0030d5e6934ae42baecb44ae20
msorber <78518576+msorber@users.noreply.github.com>
2022-06-23 10:03:49 -0400

    Create IssueSeverityIcon.vue

---
+++ library/components/IssueSeverityIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <div v-if="severity === 'critical'" class="text-[11px] bg-[#ba1515] w-[20px] h-[20px] text-[#ffffff] inline-flex items-center justify-center rounded-[2px]" title="Critical Issues">
+        C
+    </div>
+    <div v-if="severity === 'high'" class="text-[11px] bg-[#cc4f19] w-[20px] h-[20px] text-[#ffffff] inline-flex items-center justify-center rounded-[2px]" title="High Issues">
+        H
+    </div>
+    <div v-if="severity === 'medium'" class="text-[11px] bg-[#f9a11b] w-[20px] h-[20px] text-[#ffffff] inline-flex items-center justify-center rounded-[2px]" title="Medium Issues">
+        M
+    </div>
+    <div v-if="severity === 'low'" class="text-[11px] bg-[#8e8d92] w-[20px] h-[20px] text-[#ffffff] inline-flex items-center justify-center rounded-[2px]" title="Low Issues">
+        L
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IssueSeverityIcon",
+  props: {
+    severity: { 
+      type: String, 
+      required: true,
+      validator: function(value) {
+        return ["critical", "high", "medium", "low"].includes(value);
+      } 
+      },
+  },
+}
+</script>
+
+<style scoped>
+</style>
