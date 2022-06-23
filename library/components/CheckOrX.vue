4ac8a1b1fa38673082a8a1adee1762e9e02cac0e
msorber <78518576+msorber@users.noreply.github.com>
2022-06-23 11:08:15 -0400

    Create CheckOrX.vue

---
+++ library/components/CheckOrX.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="centerCheckOrX">
+    <span v-if="value" class="text-[#2D9283] mr-[6px]">
+      <i class="fas fa-check-circle"> </i>
+    </span>
+    <span v-else class="text-[#ba1515] mr-[6px]">
+      <i class="fas fa-times-circle"> </i>
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: "CheckOrX",
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.centerCheckOrX {
+  position: relative;
+  top: 4px;
+}
+</style>
