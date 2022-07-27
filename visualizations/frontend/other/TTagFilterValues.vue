784bf4a22de90bd87d4c4e0ec06151ea7e5e7962
gau94rav <gau94rav@gmail.com>
2022-07-27 15:46:54 +0530

    t tags filter values

--- visualizations/frontend/other/TTagFilterValues.vue
+++ visualizations/frontend/other/TTagFilterValues.vue
@@ -1,7 +1,6 @@
 <template>
     <t-loading-spinner v-if="loading" position="relative" />
-    <div v-else-if="items.length">
-
+    <div v-else-if="Object.keys(items).length">
         <t-grids v-if="isList" column-count="4" gap-x="8">
             <div v-if="label" class="flex-wrap text-base font-semibold whitespace-nowrap h-max">{{ label }}</div>
             <div
@@ -9,12 +8,15 @@
                 :style="{ color: textColor }"
             >
                 <div
-                    v-for="(item, index) in items"
-                    :key="item.key"
-                    class="border-b border-[#D3D3D9] w-full text-sm"
+                    v-for="(key, index) in Object.keys(items)"
+                    :key="key"
+                    class="border-b border-[#D3D3D9] w-full text-sm flex gap-1 items-center"
                     :class="index == 0 ? 'pb-2' : 'py-2'"
                 >
-                    {{ item.value }}
+                    <b>{{key}}: </b>
+                    <div class="flex flex-wrap items-center gap-1">
+                        <base-chip :text="tItem" v-for="tItem of items[key]" :key="tItem" />
+                    </div>
                 </div>
                 <span v-if="truncatedItemsCount" class="text-[#A2A1AF] py-2 text-sm">
                     &nbsp;+ {{ truncatedItemsCount }} more {{ label }}
@@ -26,14 +28,18 @@
         <t-grids v-else column-count="2" gap-x="8">
             <div v-if="label" class="flex-wrap text-base font-semibold whitespace-nowrap h-max">{{ label }}</div>
             <div
-                class="relative flex flex-wrap items-center text-base gap-x-1 whitespace-nowrap"
+                class="relative flex flex-col flex-wrap items-start justify-start gap-1 text-base whitespace-nowrap"
                 :style="{ color: textColor }"
             >
                 <div
-                    v-for="(item, index) in items"
-                    :key="item.key"
+                    v-for="(key, index) in Object.keys(items)"
+                    :key="key"
+                    class="flex gap-1"
                 >
-                    {{ item.value }}<span v-if="index < items.length - 1">{{ seperator }}</span>
+                    <b>{{key}}:</b>
+                    <div class="flex flex-wrap items-center gap-1">
+                        <base-chip :text="tItem" v-for="(tItem, tIndex) of items[key]" :key="tItem" />
+                    </div>
                 </div>
                 <span v-if="truncatedItemsCount" class="text-[#A2A1AF] py-2">
                     &nbsp;+ {{ truncatedItemsCount }} more {{ label }}
@@ -56,10 +62,6 @@
                 type: String,
                 default: '',
             },
-			seperator: {
-				type: String,
-				default: ','
-			},
             visibleItemsCount: {
                 type: Number,
                 default: -1,
@@ -84,35 +86,35 @@
                 }
                 return [];
             },
-            values() {
-                if (this.tValueColumn) {
-                    return this.getColumn(this.tValueColumn)
-                }
-                return [];
-            },
             items() {
-                const keys = this.keys;
-                const values = this.values;
-                const items = [];
-                let truncatedItems = 0;
-                
+                const keys = [...new Set(this.keys)];
+                const items = {};
                 const selectedItems = this.urlValue.split("|");
-                if (values && keys) {
-                    for (let index in keys) {
-                        if (this.visibleItemsCount > -1) {
-                            if (items.length === this.visibleItemsCount) {
-                                truncatedItems = selectedItems.length - items.length;
-                                break;
+
+                let truncatedItems = 0;
+                let addedItems = 0;
+
+                for (let key of keys) {
+                    if (this.visibleItemsCount > -1 && Object.keys(items).length === this.visibleItemsCount) {
+                        truncatedItems = selectedItems.length - addedItems;
+                        break;
+                    }
+
+                    for (let sItem of selectedItems) {
+                        const split = sItem.split('_');
+                        const k = split.length > 1 ? split[1] : split[0];
+                        if (k === key) {
+                            if (items[key]) {
+                                items[key].push(split[0])
+                            } else {
+                                items[key] = [split[0]]
                             }
                         }
-                        const key = keys[index];
-                        if (selectedItems.indexOf(key) < 0) continue;
-                        const value = values[index];
-                        items.push({ key, value });
                     }
+
+                    addedItems += 1;
                 }
                 this.truncatedItemsCount = truncatedItems;
-
                 return items;
             },
             urlValue() {
