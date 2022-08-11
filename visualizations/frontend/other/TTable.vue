3a511084eff3568c8a66a9a14fc52fdda696dfef
msorber <78518576+msorber@users.noreply.github.com>
2022-08-11 12:44:47 -0400

    Added check for undefined sql columns

--- visualizations/frontend/other/TTable.vue
+++ visualizations/frontend/other/TTable.vue
@@ -1117,6 +1117,9 @@ export default {
       });
       // remove duplicate entries
       allValidColumns = [...new Set(allValidColumns)];
+      if(allValidColumns.includes(undefined)){
+        console.error('Modify Column Configuration includes "undefined" value, please check that all sql column names are quoted!')
+      }
       thisTable.filters.column_list = JSON.stringify(allValidColumns);
       if (allValidColumns.length > 0)
         this.additionalFilters = { column_list: thisTable.filters.column_list };
