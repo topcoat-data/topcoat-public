32f0ca88ea4eb3fd9adaeed9933b3c5e7e7f5935
gau94rav <gau94rav@gmail.com>
2022-08-12 17:57:12 +0530

    removed duplicate file

--- visualizations/frontend/filters/v2/TDatePicker.vue
+++ visualizations/frontend/filters/v2/TDatePicker.vue
@@ -1,19 +1,18 @@
 <template>
     <div class="relative flex t-date-picker">
         <!-- Date Picker -->
-        {{ selectedPreset }}
         <base-date-picker
+            read-only
             show-clear-button
             confirm
             confirm-text="Apply"
             v-model="date"
             ref="datePicker"
-            :range="true"
+            :range="mode === 'range'"
             :format="dateFormat"
             :range-separator="seperator"
             @confirm="handleConfirm"
-            @close="handleClose"
-            @pick="handleDateButtonsClicked"
+            @pick="setCustomPreset"
             @open="fetchLayerData"
         >
             <div class="presets-dropown" slot="sidebar">
@@ -22,13 +21,23 @@
                     :key="preset.key"
                     :style="{ color: preset.key === selectedPreset.key ? '#1284e7' : '#73879c' }"
                     class="cursor-pointer"
-                    @click="changePreset(preset)"
+                    @click="handlePreset(preset)"
                 >
                     {{ preset.label }}
                 </div>
             </div>
 
-            <t-loading-spinner slot="icon-calendar" v-if="loading" />
+            <div slot="icon-clear"></div>
+            <div slot="icon-calendar">
+                <div class="flex items-center gap-1">
+                    <reload-icon @click="handleReload" class="cursor-pointer" />
+                    <close-icon @click="handleClear" class="cursor-pointer" />
+                    <t-loading-spinner position="relative" slot="icon-calendar" v-if="loading" />
+                    <span style="color: #1284e7" v-else>
+                        <calendar-blank-outline-icon />
+                    </span>
+                </div>
+            </div>
         </base-date-picker>
     </div>
 </template>
@@ -48,19 +57,17 @@ export default {
             type: String,
             default: " ~ ",
         },
-        hasPresetButton: {
-            type: Boolean,
-            default: false,
-        },
-        mode: {
-            type: String,
-            default: "range",
+    },
+    computed: {
+        mode() {
+            if (this.$attrs['t-filter:start_date'] && this.$attrs['t-filter:end_date']) {
+                return "range";
+            }
+            return "single";
         }
     },
     data: () => ({
         date: [],
-        previousDate: [],
-        previousPreset: {},
         presets: {
             range: [
                 { key: "last7days", label: "Last 7 Days" },
@@ -86,61 +93,167 @@ export default {
     }),
     methods: {
         onVisualizationInit() {
-            const startDate = this.getFilterValue("start_date");
-
-            if (startDate) {
-                this.date.push(new Date(startDate));
-            }
-
-            if (this.mode === "range") {
-                const endDate = this.getFilterValue("end_date");
-                if (endDate) {
-                    this.date.push(new Date(endDate));
-                }
-            }
+            this.assignDatesFromFilters();
         },
-        handleDateButtonsClicked() {
-            this.selectedPreset = { key: "custom", label: "Custom Date" };
+        setCustomPreset() {
+            this.selectedPreset = this.presets[this.mode].filter(p => p.key === "custom")[0];
         },
         handleConfirm(date) {
-            console.log(date)
-            const startDate = date[0].toISOString().slice(0,10);
-            if (startDate) {
-                this.setFilterValue("start_date", startDate);
-            }
             if (this.mode === "range") {
-                const endDate = date.length > 1 ? date[1].toISOString().slice(0,10) : null;
-                if (endDate) {
-                    this.setFilterValue("end_date", endDate);
-                }
-            }
-            
-        },
-        handleClose() {
-            // Revert to previous date if closed without clicking Apply
-            console.log(this.filters)
-            // this.date = this.previousDate;
-        },
-        changePreset(preset) {
-            if (this.mode === "range") {
-                this.handleRangeDates(preset);
+                const startDate = date[0];
+                const endDate = date[1]
+                this.setFilterValue("start_date", this.formatDate(startDate));
+                this.setFilterValue("end_date", this.formatDate(endDate));
             } else {
-                
+                this.setFilterValue("date", this.formatDate(date));
             }
+            this.setFilterValue("date_preset", this.selectedPreset.key);
+
+        },
+        handleReload() {
+            this.assignDatesFromFilters();
+            const preset = this.getFilterValue("date_preset");
+            if (preset) {
+                const presetObject = this.presets[this.mode].filter(p => p.key === preset)[0];
+                this.selectedPreset = presetObject;
+            }
+        },
+        assignDatesFromFilters() {
+            if (this.mode === "range") {
+                const date = [];
+                const startDate = this.getFilterValue("start_date");
+                const endDate = this.getFilterValue("end_date");
+
+                if (startDate) {
+                    date.push(new Date(startDate));
+                }
+
+                if (endDate) {
+                    date.push(new Date(endDate));
+                }
+
+                this.date = date;
+            } else {
+                const date = this.getFilterValue("date");
+                this.date = new Date(date);
+            }
+
+            const preset = this.getFilterValue("date_preset");
+            if (preset) {
+                const presetObject = this.presets[this.mode].filter(p => p.key === preset)[0];
+                this.selectedPreset = presetObject;
+            }
+        },
+        handlePreset(preset) {
+            if (this.mode === "range") {
+                this.handleRangeDates(preset.key);
+            } else {
+                this.handleSingleDates(preset.key);
+            }
+            this.selectedPreset = preset;
         },
         handleRangeDates(preset) {
             let startDate = new Date();
             let endDate = new Date();
-            
-            switch (preset.key) {
+
+            switch (preset) {
                 case "last7days":
                     startDate = new Date(startDate.setDate(startDate.getDate() - 7));
                     endDate = new Date(endDate.setDate(endDate.getDate() - 1));
                     break;
+                
+                case "lastmonth":
+                    const lastMonth = startDate.getMonth() - 1;
+                    startDate.setMonth(lastMonth);
+                    startDate.setDate(1); // First day of month
+                    endDate.setMonth(lastMonth);
+
+                    const lastDayOfMonth = new Date(endDate.getFullYear(), endDate.getMonth()+1, 0).getDate();
+                    endDate.setDate(lastDayOfMonth); // Last day of month
+                    break;
+                
+                case "last30days":
+                    startDate = new Date(startDate.setDate(startDate.getDate() - 30));
+                    endDate = new Date(endDate.setDate(endDate.getDate() - 1));
+                    break;
+                
+                case "last90days":
+                    startDate = new Date(startDate.setDate(startDate.getDate() - 90));
+                    endDate = new Date(endDate.setDate(endDate.getDate() - 1));
+                    break;
+                
+                case "mtd":
+                    startDate.setDate(1);
+                    break;
+                
+                case "ytd":
+                    startDate.setMonth(0);
+                    startDate.setDate(1);
+                    break;
             }
 
             this.date = [startDate, endDate];
-            this.selectedPreset = preset;
+        },
+        handleSingleDates(preset) {
+            let date = new Date();
+            switch(preset) {
+                case "yesterday":
+                    date.setDate(date.getDate() - 1);
+                    break;
+
+                case "7DaysAgo":
+                    date.setDate(date.getDate() - 7);
+                    break;
+
+                case "30DaysAgo":
+                    date.setDate(date.getDate() - 30);
+                    break;
+                
+                case "90DaysAgo":
+                    date.setDate(date.getDate() - 90);
+                    break;
+                
+                case "yearAgo":
+                    date.setDate(date.getDate() - 365);
+                    break;
+                
+            }
+            this.date = date;
+        },
+        handleClear() {
+            this.date = [];
+            this.selectedPreset = {};
+            this.unsetFilterValue("start_date");
+            this.unsetFilterValue("end_date");
+            this.unsetFilterValue("date_preset");
+            this.unsetFilterValue("date");
+        },
+        formatDate(dateValue) {
+            const date = [];
+
+            const dateObject = new Date(dateValue);
+            let year = dateObject.getFullYear();
+
+            let month = dateObject.getMonth() + 1;
+            month = month < 10 ? ("0" + month) : month;
+
+            let day = dateObject.getDate();
+            day = day < 10 ? ("0" + day) : day;
+
+            let formatItems = this.dateFormat.match(/(.)\1*/g);  // Formats the date-format like this ['YYYY', '-', 'MM', '-', 'DD']
+            for (let item of formatItems) {
+                if (item === "YYYY") {
+                    date.push(year);
+                } else if (item === "MM") {
+                    date.push(month);
+                } else if (item === "DD") {
+                    date.push(day);
+                } else {
+                    // It's a seperator
+                    date.push(item);
+                }
+            }
+            return date.join("");
         }
     },
 }
@@ -161,5 +274,9 @@ export default {
 
     .mx-datepicker-content {
         margin-left: 153px !important;
+    }
+
+    .mx-icon-calendar {
+        display: block !important;
     }
 </style>
