32d3ed0b99433ce722fd7af637e9d3813a8abe5c
Sara Regan <regan.sara@gmail.com>
2022-08-16 18:30:34 +0200

    chore: add a reset button

---
+++ visualizations/frontend/ResetAll.vue
@@ -0,0 +1,19 @@
+<template>
+<button @click="handleClick">yo it's me. A nasty button</button>
+</template>
+
+<script>
+    export default {
+        props: {
+            text: {
+                type: String,
+                default: 'nasty'
+            },
+        },
+        methods: {
+            handleClick(e) {
+                console.log('event in ResetAll', e)
+            }
+        }
+    }
+</script>
