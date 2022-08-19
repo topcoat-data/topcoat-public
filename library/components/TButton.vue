d72bbb1b27ab4002edbf6ca5798f24e83c92506f
Sara Regan <regan.sara@gmail.com>
2022-08-19 17:58:41 +0200

    chore: move button component into separate file

---
+++ library/components/TButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="text-[#145DEB]" @click="$emit('clicked')">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  emits: ['clicked'],
+  props: {
+    text: {
+      type: String,
+    },
+  },
+}
+</script>
--- visualizations/frontend/ResetAll.vue
+++ visualizations/frontend/ResetAll.vue
@@ -1,17 +1,13 @@
 <template>
-  <button @click="handleClick">{{ text }}</button>
+  <base-button variant="link" class="text-[#145DEB]" @click="handleClick">TEXT</base-button>
+  <h1>urg</h1>
 </template>
 
 <script>
 export default {
-  props: {
-    text: {
-      type: String,
-      default: 'Reset all Filters'
-    },
-  },
   methods: {
     handleClick(e) {
+      console.log('handle the cliiiiick!')
       this.unsetAllFilters()
     }
   }
