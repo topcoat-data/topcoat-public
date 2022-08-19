cc4ebf1fb35e4ef1c0d69ecb45e7f01ae4a3b6a6
msorber <78518576+msorber@users.noreply.github.com>
2022-08-19 10:06:26 -0400

    Added front-end modify columns code, including update to TTable and TSelectColumns (#108)
    * Added front-end modify columns code, including update to TTable and TSelectColumns
    
    * cleanup
    
    * Updates to make csv download use the new filters in the store state instead of grabbing them from the url and respect the user selected columns
    
    * added hidePagination prop for the pdf export since will need to fix pdf export here anyway
    
    * fixed typo
    
    * added modifiable columns filters to csv export and use the new filters state instead of grabbing from the url
    
    * Moved TSelectColumns.vue to visualizations directory
    
    * Fixed location of the modify columns/export pdf buttons when there is no title and hid the controls during pdf export
    
    * better filename generation
    
    Co-authored-by: Emiliano Castro <emiliano.e.castro@gmail.com>
    
    * Replaced button with basebutton
    
    * Added check for undefined sql columns
    
    * Updates to work with pdf export
    
    * Apply suggestions from code review
    
    Co-authored-by: Emiliano Castro <emiliano.e.castro@gmail.com>
    
    * remove console
    
    * Update library/components/TCsvExport.vue
    
    Co-authored-by: Emiliano Castro <emiliano.e.castro@gmail.com>
    
    * Fixed merge messing up vue reactivity
    
    * hide "no data" message when fetching data
    
    * Updated column prop format
    
    * cleanup
    
    * added storeAttribute call for column_list filter
    
    * use stored filters
    
    * renamed filterableColumns -> modifiableColumns
    
    * renamed exludeFromColumns -> layerColumnsToHide
    
    * Tweaked when the spinner shows and ensured that the table is taller than the spinner even when there is no data
    
    * more spinner and no data improvements
    
    * Fix bug where sometimes only 1 of many rows would show up on the first page load
    
    * added back else to avoid unnecessary recomputation
    
    * Move internalColumns from a computed property to data and manually set it.
    
    The internalColumns was being computed twice when the columns were modified, once before the rows data was properly set resulting in a console error.
    
    * added debounce on the call to update the modifiable columns
    
    Co-authored-by: Emiliano Castro <emiliano.e.castro@gmail.com>

---
+++ visualizations/frontend/other/TSelectColumns.vue
@@ -0,0 +1,173 @@
+<template>
+  <t-dropdown
+    :disable-handle-class="isExpanded"
+    :is-active="checked.length ? true : false"
+  >
+    <!-- Handle -->
+    <div
+      slot="handle"
+      class="flex items-center gap-1 p-1 text-sm font-medium"
+      v-if="!isExpanded"
+    >
+      <view-column-outline size="20" />
+      <span> Modify Columns </span>
+      <menu-down-icon size="20" />
+    </div>
+
+    <!-- Popup Contents -->
+    <div class="min-w-[294px]" attrs.slot="outside">
+      <div
+        class="
+          px-[12px]
+          pt-[16px]
+          pb-[8px]
+          flex
+          justify-between
+          items-center
+          w-full
+        "
+      >
+        <h6
+          class="
+            text-[10px] text-[#727184]
+            font-semibold
+            uppercase
+            leading-[15px]
+            tracking-widest
+          "
+        >
+          Columns
+        </h6>
+        <span
+          class="
+            text-[#145DEB] text-[13px]
+            cursor-pointer
+            font-normal
+            leading-[18px]
+          "
+          :class="checked.length ? 'text-[#145DEB]' : 'text-[#727184]'"
+        >
+          <span
+            v-if="checked.length < modifiableColumns.length"
+            @click="selectAll"
+          >
+            Select All
+          </span>
+          <span v-else @click="deselectAll"> Reset </span>
+        </span>
+      </div>
+      <div class="px-[8px] pt-[4px] pb-[6px] w-full">
+        <ul class="max-h-[320px] overflow-auto">
+          <li
+            class="
+              flex
+              justify-between
+              px-[8px]
+              pb-[1px]
+              text-sm
+              cursor-pointer
+              text-[#555463]
+            "
+            v-for="(item, index) in internalColumns"
+            :key="index"
+          >
+            <div
+              class="
+                flex
+                items-center
+                justify-between
+                w-full
+                hover:text-[#1C1C21]
+                leading-[16.41px]
+              "
+            >
+              <base-checkbox
+                class="!min-w-[200px]"
+                :label="item.label"
+                :value="item"
+                v-model="checked"
+              />
+            </div>
+          </li>
+          <small v-if="!modifiableColumns.length"> No items found </small>
+        </ul>
+      </div>
+    </div>
+  </t-dropdown>
+</template>
+
+<script>
+export default {
+  name: "TSelectColumns",
+  props: {
+    modifiableColumns: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    TLayer: {
+      type: String,
+    },
+  },
+  data: () => ({
+    checked: [],
+    search: "",
+    isExpanded: false,
+    internalColumns: [],
+  }),
+  mounted() {
+    let urlFilter = this.getFilterState(this.urlParamName);
+    const useUrlParams = typeof urlFilter === 'string';
+    if (urlFilter) {
+      urlFilter = urlFilter.split("|");
+    }
+
+    this.modifiableColumns.forEach((col) => {
+      const label = col.displayColumn;
+      const iCol = { label: label, sqlColumns: col.layerColumns };
+      this.internalColumns.push(iCol);
+      if (useUrlParams) {
+        if (urlFilter.includes(label)) this.checked.push(iCol);
+      } else if (col.displayByDefault) {
+        this.checked.push(iCol);
+      }
+    });
+    this.$emit("updateFilteredColumns", this.checked);
+  },
+  watch: {
+    checked() {
+      this.updateChecked()
+    },
+  },
+  computed: {
+    urlParamName() {
+      return `${this.TLayer}_cols`;
+    },
+  },
+  methods: {
+    selectAll() {
+      this.checked = [...this.internalColumns];
+      const visibleColumnlabels = this.checked.map((c) => c.label);
+    },
+    deselectAll() {
+      this.checked = [];
+    },
+    updateChecked: _.debounce(function(){
+      this.$emit("updateFilteredColumns", this.checked);
+      const selectColumnLabels = this.checked.map((c) => c.label);
+      this.setFilter({
+        name: this.urlParamName,
+        value: selectColumnLabels.join("|"),
+        persist: false,
+      });
+    }, 750),
+  },
+};
+</script>
+
+<style>
+.nav-search .vue--search-input__field {
+  @apply !rounded-3xl !bg-white !opacity-[0.5];
+}
+</style>
