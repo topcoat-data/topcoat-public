ebc8bbe073a94aa9bbbcdfa4855749931c84fec1
Sara Regan <regan.sara@gmail.com>
2022-08-19 17:58:41 +0200

    chore: update button

---
+++ library/components/TTextButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="
+    rounded
+    text-[#145deb]" v-on="$listeners">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ visualizations/frontend/other/TResetFilters.vue
@@ -0,0 +1,16 @@
+<template>
+  <TTextButton @click="handleClick">Reset Filters</TTextButton>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    is_filter: true
+  }),
+  methods: {
+    handleClick(e) {
+      this.unsetAllFilters()
+    }
+  }
+}
+</script>
