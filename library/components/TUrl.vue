63e83504238e0e1cc685a303b691418100e795ad
msorber <78518576+msorber@users.noreply.github.com>
2022-08-23 14:21:07 -0400

    Added prop to use/not use the default url styling and made the additionalUrlParams override the page filters when they include the same parameter

--- library/components/TUrl.vue
+++ library/components/TUrl.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :href="fullUrl" class="tUrl" :target="openInNewTab ? '_blank' : '_top'" rel="noopener noreferrer">
+  <a :href="fullUrl" :class="{ 'tUrl': includeUrlStyle }" :target="openInNewTab ? '_blank' : '_top'" rel="noopener noreferrer">
     <slot></slot>
   </a>
 </template>
@@ -25,18 +25,29 @@ export default {
     openInNewTab:{
       type: Boolean,
       default: true,
+    },
+    includeUrlStyle: {
+      type: Boolean,
+      default: true
     }
   },
   computed: {
     fullUrl() {
-      let allUrlParams = this.additionalUrlParams;
+      let allUrlParams = {...this.additionalUrlParams};
 
       if (this.includeFilterParams ) {
         const { 'context[org]': org, 'context[group]': group, ...restOfFilters } = this.getFiltersState;
 
+        // Sometimes the additional URL params will include the same filter as one of the filters already in the URL
+        // So the order of the url params below is important, any duplicated keys will end up being the last one added. 
+        // 
+        // example: in the executive summary page there is a filter for project name but in a table each row
+        // is for a project, so when the user clicks a link in that row to go to the issues-detail page, the
+        // url should go to the page with the issues-details filtered on the row's project, not the page where
+        // all of the projects selected in the filter are present.
         allUrlParams = {
-          ...allUrlParams,
-          ...restOfFilters
+          ...restOfFilters,
+          ...allUrlParams
         };
       }
 
