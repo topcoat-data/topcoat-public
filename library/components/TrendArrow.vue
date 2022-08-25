fdaf720ededf23828f64ac422a48b242df84816f
msorber <78518576+msorber@users.noreply.github.com>
2022-08-25 15:09:45 -0400

    Added TrendArrow

---
+++ library/components/TrendArrow.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="trendArrow">
+        <arrow-up-icon v-if="difference > 0" :size="iconSize" />
+        <minus-icon v-else-if="difference === 0" :size="iconSize" />
+        <arrow-down-icon v-else :size="iconSize" />
+    </div>
+</template>
+
+<script>
+export default {
+  name: "TrendArrow",
+  props: {
+    firstNumber: {
+      type: Number,
+      required: true,
+    },
+    secondNumber: {
+      type: Number,
+      required: true,
+    },
+    iconSize:{
+      type: String,
+      default: '20'
+    }
+  },
+  data: () => ({}),
+  computed:{
+      difference(){
+          return this.firstNumber - this.secondNumber
+      },
+  },
+};
+</script>
+
+<style scoped>
+    .trendArrow{
+        color: #727184;
+    }
+</style>
