1057c4f0d7785ddfe121ec2358fe8580723eb271
gau94rav <gau94rav@gmail.com>
2022-08-30 16:19:04 +0530

    handle focus and hover separately

--- library/components/TTooltip.vue
+++ library/components/TTooltip.vue
@@ -1,16 +1,22 @@
 <template>
-	<div @mouseover="handleMouseOver" @click="handleClick" ref="tooltipContainer">
+	<div
+        @mouseover="handleMouseOver"
+        @mouseleave="handleMouseLeave"
+        @click="handleClick"
+        ref="tooltipContainer"
+    >
 		<div ref="trigger">
 			<slot name="trigger">
 				<help-circle-outline-icon :size="12" />
 			</slot>
 		</div>
+
+        <!-- $slots.default is used in cases when the trigger is using v-if on content's div -->
 		<div
 			class="p-4 bg-[#1C1C21] text-white w-max h-max rounded fixed z-50"
 			:style="{ width, ...positions.tooltip }"
 			ref="tooltip"
-			v-show="showTooltip"
-			@mouseleave="handleMouseLeave"
+			v-show="showTooltip && $slots.default"
 		>
 			<slot></slot>
 			<div
@@ -32,19 +38,15 @@ export default {
 			type: String,
 			default: "top"
 		},
-		isClickable: {
-			// Force with prop
-			type: Boolean,
-			default: false
-		}
 	},
 	data: () => ({
 		positions: {},
 		showTooltip: false,
+        isFocused: false,
 	}),
 	mounted() {
 		onClickOutside(this.$refs.tooltipContainer, () => {
-			this.showTooltip = false;
+			this.hide();
 		});
 	},
 	methods: {
@@ -54,58 +56,46 @@ export default {
 			const tooltipElement = this.$refs.tooltip;
 			const tooltipPositions = {};
 			const positions = { arrow: {}, tooltip: {} };
-
 			if (triggerElement && tooltipElement) {
 				const position = triggerElement.getBoundingClientRect();
-
 				if (this.position === "top") {
-
 					positions.tooltip.top = position.top - (tooltipElement.clientHeight + 10) + "px";
 					positions.tooltip.left = position.left - tooltipElement.clientWidth / 2 + "px";
 					positions.arrow.top = position.top - 20 + "px";
 					positions.arrow.left = position.left + "px";
-
 				} else if (this.position === "bottom") {
-
 					positions.tooltip.top = position.top + triggerElement.clientHeight + 15 + "px";
 					positions.tooltip.left = position.left - tooltipElement.clientWidth / 2 + "px";
 					positions.arrow.top = position.top + triggerElement.clientHeight + 10 + "px";
 					positions.arrow.left = position.left + "px";
-
 				} else if (this.position === "left") {
-
 					positions.tooltip.top = position.top - tooltipElement.clientHeight / 2 + "px";
 					positions.tooltip.left = position.left - tooltipElement.clientWidth - 15 + "px";
 					positions.arrow.top = position.top + "px";
 					positions.arrow.left = position.left - 25 + "px";
-
 				} else {
-
 					positions.tooltip.top = position.top - tooltipElement.clientHeight / 2 + "px";
 					positions.tooltip.left = position.left + triggerElement.clientWidth + 15 + "px";
 					positions.arrow.top = position.top + "px";
 					positions.arrow.left = position.left + triggerElement.clientWidth + 10 + "px";
-
 				}
 			}
 			this.positions = positions;
 		},
 		handleMouseOver() {
-			if (this.isClickable) return;
+			if (this.isFocused) return;
 			this.show();
 		},
 		handleMouseLeave() {
-			if (this.isClickable) return;
+			if (this.isFocused) return;
 			this.hide();
 		},
 		handleClick() {
-			if (this.isClickable) {
-				this.show();
-			}
+            this.isFocused = true;
+            this.show();
 		},
 		show() {
 			this.showTooltip = true;
-
 			// Glitchy behaviour without nextTick()
 			this.$nextTick(() => {
 				this.placeTooltip();
@@ -113,6 +103,7 @@ export default {
 		},
 		hide() {
 			this.showTooltip = false;
+            this.isFocused = false;
 		}
 	}
 };
