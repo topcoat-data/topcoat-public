3fdf1fe3402123c4e6efa820f20c0a6e111a994d
Sara Regan <regan.sara@gmail.com>
2022-09-08 16:50:24 +0200

    chore: add some recommended extensions a lint file and DS_Store to gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {},
+};
