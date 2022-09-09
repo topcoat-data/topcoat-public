69074066bd162cf6b26894f98f99181643d1a9bf
gau94rav <gau94rav@gmail.com>
2022-09-09 17:31:23 +0530

    big number fixes

--- visualizations/frontend/other/TBigNumber.vue
+++ visualizations/frontend/other/TBigNumber.vue
@@ -1,174 +1,212 @@
 <template>
-    <div
-        class="w-full lg:p-5 md:p-3 p-1 rounded-[2px] break-all"
-        :style="{ background: bgColor, color: textColor }"
-        :class="hasUnderline && 'border-b-2 border-[#21214c]'"
-    >
-        <div class="flex items-center gap-2">
-            <div class="hidden opacity-50 md:block" v-if="hasIcon">
-                <slot name="icon"></slot>
-            </div>
-            <span class="text-xs font-semibold leading-4 tracking-widest">
-                {{ label || '--' }}
-            </span>
-            <base-tooltip v-if="tooltipText" :description="tooltipText" />
-        </div>
-        <div :class="hasTallSize ? 'mt-6' : 'mt-3'">
-            <span class="font-normal leading-8" :class="fontSizes[bigNumberSize]">
-                <div class="flex items-center gap-2">
-                    <span  :style="{ color: numberTextColor }">{{ value || '--' }}</span>
-                    {{ valueText }}
-                    <div v-if="previous">
-                        <span class="opacity-80" v-if="num(value) > num(previous)">
-                            <arrow-down-icon :size="18" v-if="isIncreaseBad" />
-                            <arrow-up-icon :size="18" v-else />
-                        </span>
-                        <span class="opacity-80" v-else-if="num(value) < num(previous)">
-                            <arrow-up-icon :size="18" v-if="isIncreaseBad" />
-                            <arrow-down-icon :size="18" v-else class="opacity-90" />
-                        </span>
-                        <div class="opacity-30" v-else>-</div>
-                    </div>
-                    <t-loading-spinner position="relative" v-if="loading" />
-                </div>
-            </span>
-        </div>
-        <div v-if="previous" class="flex items-center gap-1 mt-3">
-            <div class="font-semibold">
-                {{ previous }} {{ previousText }}
-            </div>
-            <div class="flex gap-2 opacity-80" v-if="percentage">
-                {{ percentage }} {{ percentageText }}
-            </div>
-            <div v-else class="opacity-80">
-                {{ noChangeText }}
-            </div>
-        </div>
-    </div>
+	<div
+		class="w-full lg:p-5 md:p-3 p-1 rounded-[2px] break-all relative"
+		:style="{ background: bgColor, color: textColor }"
+		:class="[hasUnderline && 'border-b-2 border-[#21214c]', customClass]"
+		@mouseleave="showTooltip = false"
+	>
+		<div class="relative flex items-center gap-2">
+			<div class="hidden opacity-50 md:block" v-if="$slots.icon">
+				<slot name="icon"></slot>
+			</div>
+			<span class="text-xs font-semibold leading-4 tracking-widest">
+				{{ label || "--" }}
+			</span>
+
+			<div v-show="$slots.tooltip">
+				<t-tooltip :width="tooltipWidth" :position="tooltipPosition" :open="showTooltip" @show="showTooltip = true">
+					<slot name="tooltip"></slot>
+				</t-tooltip>
+			</div>
+		</div>
+
+		<div class="mt-3">
+			<span class="font-normal leading-8" :class="fontSizes[bigNumberSize]">
+				<div class="flex items-center gap-2">
+					<t-tooltip position="right">
+						<span slot="trigger" :style="{ color: numberTextColor }">
+							{{ formatNumber(value) }} {{ valueText }}
+						</span>
+						<div
+							v-if="value.value > numberFormatLimit"
+							class="text-sm"
+						>
+							{{ value.rendered }}
+						</div>
+					</t-tooltip>
+					<div v-if="previous.value" class="opacity-80">
+						<arrow-up-icon :size="18" v-if="value.value > previous.value" />
+						<arrow-down-icon :size="18" v-else-if="value.value < previous.value" />
+						<div class="opacity-30" v-else>-</div>
+					</div>
+					<t-loading-spinner position="relative" v-if="loading" />
+				</div>
+			</span>
+		</div>
+		<div v-if="previous.value" class="flex items-center gap-1 mt-3">
+			<t-tooltip position="right" v-if="value.value != previous.value">
+				<div class="font-semibold" slot="trigger">
+					{{ formatNumber(previous) }}
+					<span class="font-normal opacity-80">{{ previousText }}</span>
+				</div>
+				<div v-if="previous.value > numberFormatLimit" class="text-sm">
+					{{ previous.rendered }}
+				</div>
+			</t-tooltip>
+			<div v-else class="opacity-80">
+				No change
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    export default {
-        props: {
-            bgColor: {
-                type: String,
-                default: 'transparent'
-            },
-            bigNumberSize: {
-                type: String,
-                default: 'medium',
-                validator: v => ['small', 'medium', 'big'].includes(v),
-            },
-            dataLoading: {
-                type: Boolean,
-                default: false,
-            },
-            numberValue: {
-                type: String,
-                default: '',
-            },
-            numberPrevious: {
-                type: String,
-                default: '',
-            },
-            label: {
-                type: String,
-                default: '',
-            },
-            textColor: {
-                type: String,
-                default: '#393842'
-            },
-            numberTextColor: {
-                type: String,
-                default: '#393842'
-            },
-            tValueColumn: {
-                type: String,
-                default: '',
-            },
-            tPreviousColumn: {
-                type: String,
-                default: '',
-            },
-            valueText: {
-                type: String,
-                default: '',
-            },
-            previousText: {
-                type: String,
-                default: '',
-            },
-            percentageText: {
-                type: String,
-                default: 'previous period',
-            },
-            noChangeText: {
-                type: String,
-                default: 'No change',
-            },
-            tooltipText: {
-                type: String,
-                default: '',
-            },
-            hasUnderline: {
-                type: Boolean,
-                default: false,
-            },
-            hasTallSize: {
-                type: Boolean,
-                default: false,
-            },
-            isIncreaseBad: {
-                type: Boolean,
-                default: false,
-            }
-        },
-        data: () => ({
-            is_filter: true,
-            fontSizes: {
-                small: 'lg:text-3xl md:text-2xl sm:text-lg',
-                medium: 'lg:text-[2.5rem] md:text-3xl sm:text-2xl',
-                big: 'lg:text-5xl md:text-4xl sm:text-3xl',
-            },
-        }),
-        computed: {
-            hasIcon() {
-                return this.$slots.icon;
-            },
-            value() {
-                if (this.numberValue) return this.numberValue;
-                const column = this.tValueColumn ? this.tValueColumn : this.findColumnByTag('value');
-                const value = this.getColumn(column)
-                return value && value.length ? value[0] : 0;
-            },
-            previous() {
-                if (!this.numberPrevious && !this.layer) return 0;
-                if (this.numberPrevious) return this.numberPrevious;
-                const column = this.tPreviousColumn ? this.tPreviousColumn : this.findColumnByTag('previous');
-                const previous = this.getColumn(column)
-                return previous && previous.length ? previous[0] : 0;
-            },
-            percentage() {
-                const value = this.num(this.value);
-                const previous = this.num(this.previous);
-                if (previous && value !== previous) {
-                    const difference = Math.abs((((value - previous) / previous) * 100).toFixed(2));
-                    let appendIncrement = '';
-                    if (this.isIncreaseBad) {
-                        appendIncrement = difference > 0 ? '-' : '+';
-                    } else {
-                        appendIncrement = difference > 0 ? '+' : '-';
-                    }
-                    return `${appendIncrement}${difference}%`;
-                }
-                return '';
-            }
-        },
-        methods: {
-            num(val) {
-                return parseInt(val.replace(/,/g, ''))
-            }
-        }
-    }
+export default {
+	props: {
+		bgColor: {
+			type: String,
+			default: "transparent"
+		},
+		bigNumberSize: {
+			type: String,
+			default: "medium",
+			validator: v => ["small", "medium", "big"].includes(v)
+		},
+		numberValue: {
+			type: Number,
+			default: ""
+		},
+		numberPrevious: {
+			type: Number,
+			default: ""
+		},
+		label: {
+			type: String,
+			default: ""
+		},
+		textColor: {
+			type: String,
+			default: "#393842"
+		},
+		tValueColumn: {
+			type: String,
+			default: ""
+		},
+		tPreviousColumn: {
+			type: String,
+			default: ""
+		},
+		valueText: {
+			type: String,
+			default: ""
+		},
+		previousText: {
+			type: String,
+			default: "previous period"
+		},
+		tooltipText: {
+			type: String,
+			default: ""
+		},
+		hasUnderline: {
+			type: Boolean,
+			default: false
+		},
+		tooltipWidth: {
+			type: String,
+			default: "200px"
+		},
+		tooltipPosition: {
+			type: String,
+			default: "top"
+		},
+		numberFormatLimit: {
+			type: Number,
+			default: 999999,
+		},
+		numberTextColor: {
+			type: String,
+			default: '#393842'
+		},
+		customClass: {
+			type: String,
+			default: "",
+		}
+	},
+	data: () => ({
+		is_filter: true,
+		fontSizes: {
+			small: "lg:text-3xl md:text-2xl sm:text-lg",
+			medium: "lg:text-[2.5rem] md:text-3xl sm:text-2xl",
+			big: "lg:text-5xl md:text-4xl sm:text-3xl"
+		},
+		showTooltip: false,
+	}),
+	computed: {
+		row() {
+			return this.rows.length ? this.rows[0] : null;
+		},
+		value() {
+			if (this.numberValue) {
+				return { value: this.numberValue, rendered: this.numberValue.toLocaleString("en") };
+			}
+
+			const column = this.tValueColumn
+				? this.tValueColumn
+				: this.findColumnByTag("value");
+
+			return this.getColumnValue(column);
+		},
+		previous() {
+			if (this.numberPrevious)
+				return { value: this.numberPrevious, rendered: this.numberPrevious.toLocaleString("en") };
+
+			const column = this.tPreviousColumn
+				? this.tPreviousColumn
+				: this.findColumnByTag("previous");
+
+			return this.getColumnValue(column);
+		},
+	},
+	methods: {
+		getColumnValue(column) {
+			// Expected column.value values: number, null, string
+			if (this.row && this.row[column]) {
+				const columbObject = this.row[column];
+
+				if (typeof columbObject.value === "string") {
+					// if '3.9'
+					columbObject.value = parseInt(columbObject.value);
+				}
+
+				if (!columbObject.value) {
+					// In some cases, value is returned null inside row object
+					columbObject.value = 0;
+				}
+
+				if (!columbObject.rendered) {
+					// In some cases, rendered is returned null inside row object
+					columbObject.rendered = "--";
+				}
+
+				return columbObject;
+			}
+
+			return {
+				value: 0,
+				rendered: "--"
+			};
+		},
+		formatNumber({ value, rendered }) {
+			// If number is greater than 6 digits, format it.
+			if (value > this.numberFormatLimit) {
+				let formatter = Intl.NumberFormat('en', { notation: 'compact' });
+				return formatter.format(value);
+			}
+
+			// Else return default rendered value from sql.
+			return rendered;
+		}
+	}
+};
 </script>
