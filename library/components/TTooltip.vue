f1bd42d7ca2e468740da493071f89d96142511bd
gau94rav <gau94rav@gmail.com>
2022-09-16 17:33:20 +0530

    correction in tooltip placement logic

--- library/components/TTooltip.vue
+++ library/components/TTooltip.vue
@@ -1,10 +1,10 @@
 <template>
-  <div
-    @mouseover="handleMouseOver"
-    @mouseleave="handleMouseLeave"
-    ref="tooltipContainer"
-  >
-    <div ref="trigger">
+  <div ref="tooltipContainer">
+    <div
+      ref="trigger"
+      @mouseleave="handleMouseLeave"
+      @mouseover="handleMouseOver"
+    >
       <slot name="trigger">
         <help-circle-outline-icon :size="12" />
       </slot>
@@ -12,17 +12,21 @@
 
     <!-- $slots.default is used in cases when the trigger is using v-if on content's div -->
     <transition name="fade">
-      <div
-        class="p-2 bg-[#1C1C21] text-white text-sm w-max h-max rounded fixed z-50"
-        :style="{ width, ...positions.tooltip }"
-        ref="tooltip"
-        v-show="isVisible && $slots.default"
-      >
-        <slot></slot>
+      <div class="relative w-0 h-0" :style="{ ...positions.tooltip }">
         <div
-          class="bg-[#1C1C21] w-4 h-4 fixed rotate-45"
-          :style="{ ...positions.arrow }"
-        ></div>
+          class="p-2 bg-[#1C1C21] text-white text-sm w-max h-max rounded absolute z-50"
+          :style="{ width }"
+          ref="tooltip"
+          v-show="isVisible && $slots.default"
+          @mouseover="handleTooltipFocus(true)"
+          @mouseleave="handleTooltipFocus(false)"
+        >
+          <slot></slot>
+          <div
+            class="bg-[#1C1C21] w-3 h-3 absolute rotate-45 bg-red"
+            :style="{ ...positions.arrow }"
+          ></div>
+        </div>
       </div>
     </transition>
   </div>
@@ -33,20 +37,22 @@ export default {
   props: {
     width: {
       type: String,
-      default: "200px"
+      default: "200px",
     },
     position: {
       type: String,
-      default: "top"
+      default: "top",
     },
-    isOpen: { // To control state from a component.
+    isOpen: {
+      // To control state from a component.
       type: Boolean,
       default: false,
-    }
+    },
   },
   data: () => ({
     positions: { arrow: {}, tooltip: {} },
     isVisible: this.isOpen,
+    isTooltipFocused: false, // If cursor is on tooltip.
   }),
   methods: {
     placeTooltip() {
@@ -56,27 +62,31 @@ export default {
 
       const positions = { ...this.positions };
       if (triggerElement && tooltipElement) {
-        const position = triggerElement.getBoundingClientRect();
         if (this.position === "top") {
-          positions.tooltip.top = position.top - (tooltipElement.clientHeight + 10) + "px";
-          positions.tooltip.left = position.left - tooltipElement.clientWidth / 2 + "px";
-          positions.arrow.top = position.top - 20 + "px";
-          positions.arrow.left = position.left + "px";
+          positions.tooltip.bottom =
+            triggerElement.clientHeight +
+            tooltipElement.clientHeight +
+            10 +
+            "px";
+          positions.tooltip.right = "25px";
+          positions.arrow.bottom = "-6px";
+          positions.arrow.left = "25px";
         } else if (this.position === "bottom") {
-          positions.tooltip.top = position.top + triggerElement.clientHeight + 15 + "px";
-          positions.tooltip.left = position.left - tooltipElement.clientWidth / 2 + "px";
-          positions.arrow.top = position.top + triggerElement.clientHeight + 10 + "px";
-          positions.arrow.left = position.left + "px";
+          positions.tooltip.top = "12px";
+          positions.tooltip.right = "25px";
+          positions.arrow.top = "-6px";
+          positions.arrow.left = "25px";
         } else if (this.position === "left") {
-          positions.tooltip.top = position.top - tooltipElement.clientHeight / 2 + "px";
-          positions.tooltip.left = position.left - tooltipElement.clientWidth - 15 + "px";
-          positions.arrow.top = position.top + "px";
-          positions.arrow.left = position.left - 25 + "px";
+          positions.tooltip.top = "-20px";
+          positions.tooltip.left =
+            "-" + (tooltipElement.clientWidth - 7) + "px";
+          positions.arrow.right = "-6px";
+          positions.arrow.top = "6px";
         } else {
-          positions.tooltip.top = position.top - tooltipElement.clientHeight / 2 + "px";
-          positions.tooltip.left = position.left + triggerElement.clientWidth + 15 + "px";
-          positions.arrow.top = position.top + "px";
-          positions.arrow.left = position.left + triggerElement.clientWidth + 10 + "px";
+          positions.tooltip.top = "-20px";
+          positions.tooltip.left = triggerElement.clientWidth + 7 + "px";
+          positions.arrow.left = "-6px";
+          positions.arrow.top = "5px";
         }
       }
       this.positions = positions;
@@ -93,29 +103,41 @@ export default {
       }
       this.hide();
     },
+    handleTooltipFocus(isFocused) {
+      this.isTooltipFocused = isFocused;
+      if (!isFocused) {
+        this.hide();
+      }
+    },
     show() {
       this.isVisible = true;
 
       this.$nextTick(() => {
         // Without $nextTick, behaviour is glitched.
         this.placeTooltip();
-      })
+      });
     },
-    hide() {
+    hide: window._.debounce(function () {
+      // Debounce will give user chance to hover to tooltip and keep it open, as long as cursor stays on it.
+      // This is useful when tooltip contains clickable links.
+      if (this.isTooltipFocused) {
+        // If user is interacting with tooltip
+        return;
+      }
       this.isVisible = false;
-    }
+    }, 100),
   },
 };
 </script>
 
 <style>
-  .fade-enter-active,
-    .fade-leave-active {
-    transition: opacity 0.5s ease;
-    }
-  
-    .fade-enter-from,
-    .fade-leave-to {
-    opacity: 0;
-    }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
