a8252f8a7fcfc4b8dce52b77d323f42f62dde2ab
gau94rav <gau94rav@gmail.com>
2022-10-07 14:32:18 +0530

    filter section component

--- library/templates/Report/Report.vue
+++ library/templates/Report/Report.vue
@@ -1,47 +1,50 @@
 <template>
-    <div>
-        <report-header v-if="hasHeader" />
-        <div class="bg-[#F9F9FA]">
-            <div class="flex w-full gap-10 content-block">
-                <div class="w-full">
-                    <div class="flex flex-wrap justify-between items-center w-full py-4 px-6 border-b border-[#E4E3E8] md:flex-row flex-col gap-1">
-                        <slot name="filters"></slot>
-                    </div>
-                    <div class="w-full px-6 py-5">
-                        <slot name="mid"></slot>
-                    </div>
-                    <div class="py-5 px-6 bg-[#ffffff] border-t border-[#E4E3E8]">
-                        <slot></slot>
-                    </div>
-                </div>
-            </div>
+  <div>
+    <report-header v-if="hasHeader" />
+    <div class="bg-[#F9F9FA]">
+      <div class="flex w-full gap-10 content-block">
+        <div class="w-full">
+          <t-filter-section slot="filters" />
+          <div
+            class="flex flex-wrap justify-between items-center w-full py-4 px-6 border-b border-[#E4E3E8] md:flex-row flex-col gap-1"
+          >
+            <slot name="filters"></slot>
+          </div>
+          <div class="w-full px-6 py-5">
+            <slot name="mid"></slot>
+          </div>
+          <div class="py-5 px-6 bg-[#ffffff] border-t border-[#E4E3E8]">
+            <slot></slot>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style>
-  body {
-    @apply bg-[#F6F7FB];
-  }
+body {
+  @apply bg-[#F6F7FB];
+}
 </style>
 
 <script>
-  export default {
-    data:()=>({
-      is_filter:true,
-      popup:false
-    }),
-    props: {
-      // to hide the header use this prop
-      // the header is shown only if the report is not shown in an iframe
-      hasHeader: {
-        type: Boolean,
-        default: () => window.location === window.parent.location,
-      },
-      showSidebar: {
-        type: Boolean,
-        default: false,
-      }
+export default {
+  data: () => ({
+    is_filter: true,
+    popup: false,
+  }),
+  props: {
+    // to hide the header use this prop
+    // the header is shown only if the report is not shown in an iframe
+    hasHeader: {
+      type: Boolean,
+      default: () => window.location === window.parent.location,
     },
-  };
+    showSidebar: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
 </script>
