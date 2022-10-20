7d25f90387938b40410ab70f92600a31de52fc22
gau94rav <gau94rav@gmail.com>
2022-10-20 17:16:53 +0530

    filter section components

---
+++ library/components/TFiltersSection.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    class="border-t border-b border-[#D3D3D9] w-full relative p-5 flex flex-col gap-2"
+    style="font-family: 'Roboto'"
+  >
+    <!-- Header -->
+    <div class="flex items-center gap-4 text-[#555463]">
+      <div class="flex gap-1 hover:text-[#145DEB]">
+        <filter-variant-icon :size="18" />
+        Filters
+      </div>
+      <div class="flex gap-1 hover:text-[#145DEB]">
+        <reload-icon :size="18" />
+        Reset
+      </div>
+    </div>
+    <div class="flex flex-wrap items-center gap-1">
+      <component
+        v-for="(visible, tag) in filterComponents"
+        :key="tag"
+        :is="tag"
+        v-if="visible"
+      />
+      <t-filters-dropdown :items="items" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    filterComponents: {},
+  }),
+  computed: {
+    items() {
+      let items = {};
+      if (this.$slots.default) {
+        this.$slots.default.forEach((element, index) => {
+          if (element.data && element.componentOptions) {
+            const attrs = element.data.attrs || {};
+            const label =
+              element?.componentOptions?.propsData?.label || attrs["label"];
+            const type = attrs["dropdown-section"] || "Ungrouped";
+            // Check active url filters
+            let urlParams = [];
+            let visible = false;
+            for (let attribute of Object.keys(attrs)) {
+              if (attribute.includes("t-filter")) {
+                if (this.filters.hasOwnProperty(attrs[attribute])) {
+                  urlParams = [];
+                  visible = true;
+                  break;
+                }
+                urlParams.push(attrs[attribute]);
+              }
+            }
+            // Create component instance
+            const componentTag = `${label}${index}`.replace(/\s/g, "");
+            if (!this.filterComponents.hasOwnProperty(componentTag)) {
+              window.Vue.component(componentTag, {
+                render: function (c) {
+                  return c("div", {}, [element]);
+                },
+              });
+            }
+            // Handle visibility state
+            // If filter is used as url param, show the filter in active section.
+            this.$set(this.filterComponents, componentTag, visible);
+            // If filter is not used as url param, show in dropdown.
+            if (urlParams.length) {
+              if (items[type]) {
+                items[type].items = [
+                  ...items[type].items,
+                  { label, filters: urlParams },
+                ];
+              } else {
+                items[type] = {
+                  label: type,
+                  items: [{ label, filters: urlParams }],
+                };
+              }
+            }
+          }
+        });
+      }
+      return items;
+    },
+  },
+};
+</script>
---
+++ visualizations/frontend/other/TFiltersDropdown.vue
@@ -0,0 +1,131 @@
+<template>
+  <t-dropdown>
+    <!-- Handle button -->
+    <div slot="handle" class="flex items-center gap-1 p-1 text-sm font-medium">
+      <plus-icon :size="18" />
+      Add filter
+      <menu-down-icon :size="18" />
+    </div>
+
+    <!-- Search input -->
+    <div class="p-2 filters-search roboto-fonts">
+      <div class="search-input">
+        <magnify-icon :size="18" />
+        <input
+          class="bg-transparent outline-none !text-black"
+          placeholder="Filters by..."
+          v-model="search"
+        />
+        <div class="w-5">
+          <close-icon v-if="search" :size="18" @click="search = ''" />
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-2 px-2 pb-2 tracking-widest roboto-fonts">
+      <div v-if="!filteredItems.length">No filters found</div>
+      <div v-for="(section, index) in filteredItems" :key="section">
+        <div class="px-2 py-1 text-xs font-semibold">{{ section.label }}</div>
+        <div class="text-sm">
+          <div
+            class="px-2 py-1 rounded hover:bg-[#F9F8FA]"
+            v-for="item in expanded.includes(index)
+              ? section.items
+              : section.items.slice(0, itemLimit)"
+            :key="item"
+            @click="toggleFilter(item.filters)"
+          >
+            {{ item.label }}
+          </div>
+        </div>
+        <div
+          @click="handleExpanded(index)"
+          class="text-[#145DEB] text-sm px-2 py-1"
+          v-if="section.items.length > 4"
+        >
+          <span v-if="expanded.includes(index)"> Show less </span>
+          <span v-else>
+            Show all {{ section.label.toLowerCase() }} filters
+          </span>
+        </div>
+      </div>
+    </div>
+  </t-dropdown>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: [],
+    },
+    itemLimit: {
+      type: Number,
+      default: 4,
+    },
+  },
+  data: () => ({
+    search: "",
+    is_filter: true,
+    expanded: [],
+  }),
+  computed: {
+    filteredItems() {
+      // Show filters according to `search` & `filters` variables.
+      const items = [];
+      const usedFilters = [];
+      for (let key of Object.keys(this.items)) {
+        let filteredItems = [];
+        const section = this.items[key];
+        for (let item of section.items) {
+          // Only include searched items.
+          if (
+            item.label &&
+            !item.label.toLowerCase().includes(this.search.toLowerCase())
+          ) {
+            continue;
+          }
+          filteredItems.push(item);
+        }
+        if (!filteredItems.length) {
+          continue;
+        }
+        items.push({
+          label: section.label,
+          items: filteredItems,
+        });
+      }
+      return items;
+    },
+  },
+  methods: {
+    toggleFilter(filters) {
+      for (let filter of filters) {
+        this.setFilter({
+          name: filter,
+          value: "",
+        });
+      }
+    },
+    handleExpanded(index) {
+      if (this.expanded.includes(index)) {
+        return (this.expanded = this.expanded.filter((e) => e !== index));
+      }
+      return this.expanded.push(index);
+    },
+  },
+};
+</script>
+
+<style>
+.search-input {
+  @apply border text-[#A2A1AF] border-[#B3B2BD] flex gap-3 items-center bg-white p-2 rounded-3xl h-8 tracking-widest;
+}
+.search-input:hover {
+  @apply text-[#145DEB] border-[#145DEB];
+}
+.roboto-fonts {
+  font-family: "Roboto";
+}
+</style>
