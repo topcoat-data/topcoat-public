a96d3f26d9617bcea153f73e473bf018d5cdbf44
Gaurav Singh <43262405+gau94rav@users.noreply.github.com>
2022-10-24 14:37:46 +0530

    Key value pair format change (#159)
    * replace string based key val pairs with object
    
    * change error logs to use error func

--- visualizations/frontend/filters/v2/TTagsInput.vue
+++ visualizations/frontend/filters/v2/TTagsInput.vue
@@ -1,145 +1,144 @@
 <template>
-	<div class="t-tags-input max-w-max min-w-full min-h-[43px] relative">
-		<div class="pb-2 font-bold tracking-widest" v-if="label">
-			{{ label.toUpperCase() }}
-		</div>
-		<base-multiple-value-input
-			v-if="isSingleTag"
-			v-model="selectedTags"
-			selection-only
-			:data="tags.length ? tags : tempTags"
-			:loading="loading"
-			:placeholder="placeholder"
-			@update="handleUpdate"
-			display-key="value"
-			value-key="value"
-			@opened="handleOpen"
-		/>
-		<base-tags-input
-			v-else
-			v-model="selectedTags"
-			selection-only
-			:data="tags"
-			:loading="loading"
-			:placeholder="placeholder"
-			@update="handleUpdate"
-			@opened="handleOpen"
-		/>
-	</div>
+  <div class="t-tags-input max-w-max min-w-full min-h-[43px] relative">
+    <div class="pb-2 font-bold tracking-widest" v-if="label">
+      {{ label.toUpperCase() }}
+    </div>
+    <base-multiple-value-input
+      v-if="isSingleTag"
+      v-model="selectedTags"
+      selection-only
+      :data="tags.length ? tags : tempTags"
+      :loading="loading"
+      :placeholder="placeholder"
+      @update="handleUpdate"
+      display-key="value"
+      value-key="value"
+      @opened="handleOpen"
+    />
+    <base-tags-input
+      v-else
+      v-model="selectedTags"
+      selection-only
+      :data="tags"
+      :loading="loading"
+      :placeholder="placeholder"
+      @update="handleUpdate"
+      @opened="handleOpen"
+    />
+  </div>
 </template>
 
 <script>
-	export default {
-		props: {
-			label: {
-				type: String,
-				default: '',
-			},
-			tKeyColumn: {
-				type: String,
-				default: 'KEY',
-			},
-			tValueColumn: {
-				type: String,
-				default: 'VALUE',
-			},
-			isSingleTag: {
-				type: Boolean,
-				default: false,
-			},
-			placeholder: {
-				type: String,
-				default: '',
-			}
-		},
-		data: () => ({
-			is_filter: true,
-			selectedTags: [],
-			tempTags: [], // base-multiple-value-input does not show selected items if `tags` is empty because data is fetched on open.
-		}),
-		computed:{ 
-			tags() {
-				const items = [];
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    tKeyColumn: {
+      type: String,
+      default: "KEY",
+    },
+    tValueColumn: {
+      type: String,
+      default: "VALUE",
+    },
+    isSingleTag: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+  data: () => ({
+    is_filter: true,
+    selectedTags: [],
+    tempTags: [], // base-multiple-value-input does not show selected items if `tags` is empty because data is fetched on open.
+  }),
+  computed: {
+    tags() {
+      const items = [];
 
-				if (this.rows.length) {
-					for (let row of this.rows) {
-						const key = row[this.tKeyColumn];
-						const value = row[this.tValueColumn];
-						if (key && value) {
-							items.push({
-								key: key.value,
-								value: value.value,
-							})
-						}
-					}
-				}
-				return items;
-			},
-			firstKey() {
-				// Used for single tag
-				if (this.tags.length) {
-					return this.tags[0].key;
-				}
-				
-				if (this.tempTags) {
-					return this.tempTags[0].key;
-				}
-				
-				return null;
-			}
-		},
-		methods: {
-			onVisualizationInit() {
-				const initial_value = this.getFilterValue("selected_items");
+      if (this.rows.length) {
+        for (let row of this.rows) {
+          const key = row[this.tKeyColumn];
+          const value = row[this.tValueColumn];
+          if (key && value) {
+            items.push({
+              key: key.value,
+              value: value.value,
+            });
+          }
+        }
+      }
+      return items;
+    },
+    firstKey() {
+      // Used for single tag
+      if (this.tags.length) {
+        return this.tags[0].key;
+      }
 
-				if (initial_value) {
-					const selectedTags = [];
-                    const tempTags = [];
-					const urlTags = initial_value.split('|');
-					
-					for (let item of urlTags) {
-						const splitPair = item.split('_');
-						const key = splitPair.length > 1 ? splitPair[1] : splitPair[0];
-						const value = splitPair[0];
+      if (this.tempTags) {
+        return this.tempTags[0].key;
+      }
 
-						if (key && value) {
-							if (this.isSingleTag) {
-								selectedTags.push(value);
-								tempTags.push({ key, value })
-							} else {
-								selectedTags.push({ key, value });
-							}
-						}
-					}
-					
-				    this.tempTags = tempTags;
-				    this.selectedTags = selectedTags;
-				}
-			},
-			handleUpdate(items) {
-				const values = [];
-				for (let item of items) {
-					if (this.isSingleTag) {
-						if (!this.firstKey) {
-							break;
-						}
-						values.push(`${item}_${this.firstKey}`);
-					} else {
-						values.push(`${item.value}_${item.key}`);
-					}
-				}
-				this.setFilterValue("selected_items", values.join('|'));
-			},
-			handleOpen() {
-				this.fetchLayerData();
-			},
-		},
-	}
+      return null;
+    },
+  },
+  methods: {
+    onVisualizationInit() {
+      const initial_value = this.getFilterValue("selected_items");
+      if (initial_value) {
+        try {
+          const urlTags = JSON.parse(initial_value);
+          for (const [key, value] of Object.entries(urlTags)) {
+            let tags = value.map((v) => {
+              return {
+                key,
+                value: v,
+              };
+            });
+            if (this.isSingleTag) {
+              // base-multiple-value-input is used for single tag filter.
+              this.tempTags = [...tags, ...this.tempTags]; // Without defaults base-multiple-value-input cannot show selected tags.
+              this.selectedTags = [...value, ...this.selectedTags]; // base-multiple-value-input needs exact value instead of key:val pairs.
+            } else {
+              this.selectedTags = [...tags, ...this.selectedTags];
+            }
+          }
+        } catch (error) {
+          console.error(error);
+          console.error("Failed to parse tags data");
+        }
+      }
+    },
+    handleUpdate(items) {
+      const values = {};
+      for (let item of items) {
+        if (this.isSingleTag) {
+          if (!this.firstKey) {
+            break;
+          }
+          values[this.firstKey] = [item, ...(values[this.firstKey] || [])];
+        } else {
+          values[item.key] = [item.value, ...(values[item.key] || [])];
+        }
+      }
+      this.setFilterValue("selected_items", JSON.stringify(values));
+    },
+    handleOpen() {
+      this.fetchLayerData();
+    },
+  },
+};
 </script>
 
 <style>
-	.t-tags-input .vue--multiple-value-input__body {
-		position: relative !important;
-		min-width: 100% !important;
-	}
+.t-tags-input .vue--multiple-value-input__body {
+  position: relative !important;
+  min-width: 100% !important;
+}
 </style>
