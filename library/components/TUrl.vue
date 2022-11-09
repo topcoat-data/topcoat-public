5b3f49300aef9f2d9a0445f8a03522258d9db39f
gau94rav <gau94rav@gmail.com>
2022-11-09 17:42:23 +0530

    double encoding

--- library/components/TUrl.vue
+++ library/components/TUrl.vue
@@ -39,6 +39,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    isDoubleEncoded: {
+      type: Boolean,
+      default: false,
+    },
   },
   computed: {
     fullUrl() {
@@ -72,9 +76,16 @@ export default {
         };
       }
 
-      const urlParamString = new URLSearchParams(allUrlParams)
-        .toString()
-        .replace(/\+/g, "%2520");
+      let urlParamString = new URLSearchParams(allUrlParams).toString();
+
+      if (this.isDoubleEncoded) {
+        const params = new URLSearchParams();
+        for (let key in allUrlParams) {
+          params.set(key, encodeURIComponent(allUrlParams[key]));
+        }
+
+        urlParamString = params.toString();
+      }
 
       if (urlParamString.length > 0) {
         return `${this.url}?${urlParamString}`;
