56e651cf9da77c10f2f009a60ff2ac70f0562192
Gaurav Singh <43262405+gau94rav@users.noreply.github.com>
2022-12-22 00:09:06 +0530

    TCustomHtml component (#191)

---
+++ visualizations/frontend/other/TCustomHtml.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="relative">
+    <slot :rows="rows"> Empty custom HTML visualization </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    is_filter: true,
+    // Is filter removes height and background css to allow for custom html and css changes.
+  }),
+};
+</script>
