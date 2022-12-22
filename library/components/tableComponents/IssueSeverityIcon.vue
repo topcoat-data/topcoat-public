72670d341b61caedadba699e9c684ee83f894a8d
msorber <margaret.sorber@snyk.io>
2022-12-22 00:59:34 -0500

    Add support for numerical issue severities

--- library/components/tableComponents/IssueSeverityIcon.vue
+++ library/components/tableComponents/IssueSeverityIcon.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
     <div
-      v-if="severity === 'Critical'"
+      v-if="['Critical', 4].includes(severity)"
       class="text-[11px] bg-[#ba1515] w-[20px] h-[20px] text-[#ffffff] inline-flex items-center justify-center rounded-[2px]"
       title="Critical Issues"
     >
       C
     </div>
     <div
-      v-if="severity === 'High'"
+      v-else-if="['High', 3].includes(severity)"
       class="text-[11px] bg-[#cc4f19] w-[20px] h-[20px] text-[#ffffff] inline-flex items-center justify-center rounded-[2px]"
       title="High Issues"
     >
       H
     </div>
     <div
-      v-if="severity === 'Medium'"
+      v-else-if="['Medium', 2].includes(severity)"
       class="text-[11px] bg-[#f9a11b] w-[20px] h-[20px] text-[#ffffff] inline-flex items-center justify-center rounded-[2px]"
       title="Medium Issues"
     >
       M
     </div>
     <div
-      v-if="severity === 'Low'"
+      v-else-if="['Low', 1].includes(severity) "
       class="text-[11px] bg-[#8e8d92] w-[20px] h-[20px] text-[#ffffff] inline-flex items-center justify-center rounded-[2px]"
       title="Low Issues"
     >
@@ -36,10 +36,10 @@ export default {
   name: "IssueSeverityIcon",
   props: {
     severity: {
-      type: String,
+      type: [String, Number],
       required: true,
       validator: function (value) {
-        return ["Critical", "High", "Medium", "Low"].includes(value);
+        return ["Critical", "High", "Medium", "Low", 1, 2, 3, 4].includes(value);
       },
     },
   },
