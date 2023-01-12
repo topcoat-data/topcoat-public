fa536aae1426fe26e2e70a4fd0def4368d69e845
msorber <margaret.sorber@snyk.io>
2023-01-12 15:01:23 -0500

    Initial dropdown menu

--- library/components/tableComponents/SnykPager.vue
+++ library/components/tableComponents/SnykPager.vue
@@ -4,6 +4,37 @@
       v-if="numberOfItems && numberOfItems > itemsPerPage"
       class="pagination-footer flex justify-center relative"
     >
+      <!-- Rows Per Page -->
+      <t-dropdown>
+        <!-- Handle -->
+        <div
+          slot="handle"
+          class="flex items-center gap-1 p-1 text-sm font-medium"
+        >
+          <span>{{ itemsPerPage }} Per Page </span>
+          <menu-down-icon size="20" />
+        </div>
+
+        <!-- Popup Contents -->
+        <div class="min-w-[294px]" attrs.slot="outside">
+          <div class="px-[8px] pt-[4px] pb-[6px] w-full">
+            <ul class="max-h-[320px] overflow-auto">
+              <li
+                v-for="itemsPerPageOption in [10, 50, 100, 500, 1000]"
+                :key="itemsPerPageOption"
+                class="flex justify-between px-[8px] pb-[1px] text-sm cursor-pointer text-[#555463]"
+              >
+                <div
+                  class="flex items-center justify-between w-full hover:text-[#1C1C21] leading-[16.41px]"
+                >
+                  {{ itemsPerPageOption }}
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </t-dropdown>
+
       <ul
         id="pages"
         class="pagination-nav pagination-selector inline-flex pt-[16px]"
@@ -16,7 +47,11 @@
             <i class="fas fa-arrow-left"></i>
           </button>
         </li>
-        <li v-for="page in pageNumbersToShow" class="pagination-nav-item">
+        <li
+          v-for="page in pageNumbersToShow"
+          :key="page"
+          class="pagination-nav-item"
+        >
           <button
             class="h-8 minWidth text-[15px] font-medium leading-5 focus:outline-none"
             :class="[
@@ -41,8 +76,8 @@
       <div
         class="absolute right-2 top-5 font-normal text-[#727184] text-[13px] leading-[18px]"
       >
-        Showing {{ startIndex + 1 }}- 
-        {{  Math.min(startIndex + itemsPerPage, numberOfItems) }}
+        Showing {{ startIndex + 1 }}-
+        {{ Math.min(startIndex + itemsPerPage, numberOfItems) }}
         of {{ numberOfItems.toLocaleString() }}
       </div>
     </div>
@@ -64,7 +99,7 @@ export default {
   },
   computed: {
     pageNumbersToShow() {
-      pageNumbersToShow = [];
+      let pageNumbersToShow = [];
       let pageToAdd = this.internalPage;
       let numberOfPagesAdded = 0;
 
