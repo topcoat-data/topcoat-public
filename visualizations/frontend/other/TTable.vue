221d57327c1e2c48ad7661ff787f769ebe6e5dac
msorber <78518576+msorber@users.noreply.github.com>
2023-02-03 10:46:06 -0500

    Fix/bugs in modifiable rows per page (#197)
    * Only allow valid rows per page values
    
    * Set rows per page to 50 for pdfs

--- visualizations/frontend/other/TTable.vue
+++ visualizations/frontend/other/TTable.vue
@@ -537,7 +537,17 @@ export default {
   mounted() {
     this.fetchTotalRows();
 
-    this.internalRowsPerPage = this.getFilterState("tableRowsPerPage") || this.rowsPerPage;
+    if (this.isPdf) {
+      this.internalRowsPerPage = 50;
+    } else {
+      this.internalRowsPerPage =
+        this.getFilterState("tableRowsPerPage") || this.rowsPerPage;
+      // Only allow valid rows per pages regardless of the url value or prop
+      if (!this.rowsPerPageOptions.includes(this.internalRowsPerPage)) {
+        this.internalRowsPerPage = this.rowsPerPageOptions[0];
+        this.setUrlFilter("tableRowsPerPage", this.internalRowsPerPage);
+      }
+    }
 
     const modifiableColumnsFilter = this.getFilterState(
       this.modifiableColumnsFilterName
