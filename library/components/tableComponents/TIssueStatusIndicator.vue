d4efde7a8a5f75236634ca431d238492f578aca7
msorber <margaret.sorber@snyk.io>
2023-03-30 13:04:11 -0400

    Updated the color of ignored issues to gray

--- library/components/tableComponents/TIssueStatusIndicator.vue
+++ library/components/tableComponents/TIssueStatusIndicator.vue
@@ -3,11 +3,14 @@
     <div class="text-[10px] text-normal leading-[17px]">
       <span
         class="badge-error rounded px-[6px] py-1 border text-[10px] font-medium tracking-[0.02em] uppercase leading-[11px]"
-        :class="[
-          status.toLowerCase() === 'open'
-            ? 'bg-[#FFEEF2] border-[#F15366] text-[#B9002C]'
-            : 'bg-[#EDFFFB]  border-[#52F2E0] text-[#00BA8F]',
-        ]"
+        :class="{
+          'bg-[#FFEEF2] border-[#F15366] text-[#B9002C]':
+            status.toLowerCase() === 'open',
+          'bg-[#EDFFFB] border-[#52F2E0] text-[#00BA8F]':
+            status.toLowerCase() === 'resolved',
+          'bg-[#f0f0f0] border-[#1f1f1f] text-[#1f1f1f]':
+            status.toLowerCase() === 'ignored',
+        }"
       >
         {{ status.toUpperCase() }}
       </span>
