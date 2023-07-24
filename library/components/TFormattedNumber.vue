daf8a14046315634f976e49ce8727ae9ff8b4ed9
sareg0 <regan.sara@gmail.com>
2023-07-24 15:56:27 +0200

    chore: create number formatting component

---
+++ library/components/TFormattedNumber.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>{{ formattedNumber }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+    truncationLimit: {
+      type: Number,
+      default: 999999,
+    },
+  },
+  computed: {
+    formattedNumber() {
+      if (this.value > this.truncationLimit) {
+        return Intl.NumberFormat("en", { notation: "compact" }).format(
+          this.value
+        );
+      }
+
+      return Intl.NumberFormat("en").format(this.value);
+    },
+  },
+};
+</script>
