3e6fb11954c8c75646a4cff5cd9b6924f6cc88e5
Gaurav Singh <43262405+gau94rav@users.noreply.github.com>
2023-09-07 13:14:09 +0530

    ajax request watcher and pdf component changes (#269)
    * ajax request watcher and pdf component changes
    
    * filter out layers requests

---
+++ library/components/TPdfDownloadHelper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-if="isVisible" id="seleniumComponent" class="fixed top-0 bottom-0" />
+</template>
+
+<script>
+export default {
+  data: () => ({
+    responses: 0,
+    isVisible: false,
+  }),
+  watch: {
+    responses: _.debounce(function (newVal) {
+      this.showSeleniumComponent();
+    }, 1000),
+  },
+  created() {
+    this.countAjaxResponses();
+  },
+  methods: {
+    countAjaxResponses() {
+      var $this = this;
+      axios.interceptors.response.use(
+        function (response) {
+          if (response.config.url.includes("/api/layers")) {
+            $this.responses += 1;
+          }
+          return response;
+        },
+        function (error) {
+          if (error.config.url.includes("/api/layers")) {
+            $this.responses += 1;
+          }
+          return Promise.reject(error);
+        }
+      );
+    },
+    showSeleniumComponent() {
+      this.isVisible = true;
+    },
+  },
+};
+</script>
